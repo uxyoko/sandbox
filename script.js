a173f0333951bf463eda2f563a67d2eac2fefab3
Jeff <jeffahking@gmail.com>
2017-08-12 14:20:38 -0400

    add listener to img

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('hi!')
+let catPicture = document.querySelector('img')
+
+catPicture.addEventListener('click', listenForClick)
+
+function listenForClick() {
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+	console.log('CAT!')
+}
+
+console.log(catPicture)
